67caf4658687275d56fb76e6bc360758feca63ff
Matt <matt.leonard.22@gmail.com>
2017-02-20 15:14:34 -0800

    added titles and started on scales

--- js/Scatterplot.js
+++ js/Scatterplot.js
@@ -9,16 +9,20 @@
             var json = JSON.parse(xhr.response);
 
             function buildChart(dataset) {
-                console.log(dataset);
                 // Variable instantiations 
                 var width = "600";
                 var height = "400";
-
+                var padding = "40";
+                console.log(dataset);
                 // Set up xScale
-                
+                var xScale = d3.scaleLinear()
+                                     .domain()
+                                     .range();
                 
                 // Set up yScale              
-                
+                var yScale = d3.scaleLinear()
+                                     .domain([0, 34])
+                                     .range(0, height);
 
                 // Append svg to #chartDiv div
                 var svg = d3.select("#chartDiv")
@@ -33,9 +37,16 @@
                    .enter()
                    .append("circle")
                    .attr("cx", function(d, i) { return (i * 10) + 50; })
-                   .attr("cy", function(d, i) { return height - (i * 2) - 50; })
+                   .attr("cy", function(d, i) { return 34 - d.Place + 100; })
                    .attr("r", function(d, i) { return 3; })
-                   .attr("fill", "blue");
+                   .attr("fill", "blue")
+                   .append("title")
+                   // Figure out how to format this nicely with all the info in it
+                   .text(function(d) { return d.Name + ": " + d.Nationality })
+                   .attr("x", function(d) { return 0; })
+                   .attr("y", function(d) { return 50; })
+                   .attr("font-size", "12px")
+                   .attr("fill", "black")
                    
                 
                 // Append x axis label
