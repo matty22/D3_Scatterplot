001326ef772ad5bcdeacb6e224e4bd5b978edda6
Matt <matt.leonard.22@gmail.com>
2017-03-04 14:52:36 -0800

    rewrote scatterplot from scratch

--- js/Scatterplot.js
+++ js/Scatterplot.js
@@ -1,7 +1,5 @@
 // Scatterplot.js
 
-
-
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json');
     xhr.onload = function() {
@@ -9,57 +7,125 @@
             var json = JSON.parse(xhr.response);
 
             function buildChart(dataset) {
-                // Variable instantiations 
-                var width = "600";
-                var height = "400";
-                var padding = "40";
-                console.log(dataset);
-                // Set up xScale
+
+                // Instantiate variables
+                var svgWidth = 700;
+                var svgHeight = 500;
+                var chartPadding = 40;
+                var dataset = json
+
+                // Add seconds behind best time property to each JSON object
+                dataset.forEach(function(finish) {
+                    finish.Behind = finish.Seconds - 2210;
+                });
+
+                // Set up scales
                 var xScale = d3.scaleLinear()
-                                     .domain()
-                                     .range();
+                               .domain([0, d3.max(dataset, function(d) { return d.Behind + 20; })])
+                               .range([svgWidth - chartPadding, chartPadding]);
                 
-                // Set up yScale              
                 var yScale = d3.scaleLinear()
-                                     .domain([0, 34])
-                                     .range(0, height);
+                               .domain([1, d3.max(dataset, function(d) { return d.Place + 1; })])
+                               .range([chartPadding, svgHeight - chartPadding]);
 
-                // Append svg to #chartDiv div
-                var svg = d3.select("#chartDiv")
-                            .append("svg")
-                            .attr("width", width)
-                            .attr("height", height)
-                            .attr("class", "chartBackground");
 
-                // Append all circles to svg
-                svg.selectAll("circles")
+                // Set up xAxis
+                var xAxis = d3.axisBottom(xScale);
+
+                // Set up yAxis
+                var yAxis = d3.axisLeft(yScale);
+                
+
+                // Create svg and append to DOM
+                var svg = d3.select('#chartDiv')
+                            .append('svg')
+                            .attr('width', svgWidth)
+                            .attr('height', svgHeight)
+                            .attr('class', 'chartBackground')
+
+                // Setup tooltip div
+                var tooltip = d3.select('body')
+                                .append('div')
+                                .attr('class', 'tooltipStyles')
+
+                // Add circles to the svg
+                svg.selectAll('circle')
                    .data(dataset)
                    .enter()
-                   .append("circle")
-                   .attr("cx", function(d, i) { return (i * 10) + 50; })
-                   .attr("cy", function(d, i) { return 34 - d.Place + 100; })
-                   .attr("r", function(d, i) { return 3; })
-                   .attr("fill", "blue")
-                   .append("title")
-                   // Figure out how to format this nicely with all the info in it
-                   .text(function(d) { return d.Name + ": " + d.Nationality })
-                   .attr("x", function(d) { return 0; })
-                   .attr("y", function(d) { return 50; })
-                   .attr("font-size", "12px")
-                   .attr("fill", "black")
-                   
+                   .append('circle')
+                   .attr('cx', function(d, i) { return xScale(d.Behind); })
+                   .attr('cy', function(d) { return yScale(d.Place); })
+                   .attr('r', 5)
+                   .attr('class', function(d) {
+                       if (d.Doping) {
+                           return 'isDoper'
+                       } else {
+                           return 'isNotDoper'
+                       }
+                   })
+                   .on('mouseover', function(d) {
+                       tooltip.transition().style('display', 'block')
+                       tooltip.html(d.Name + ', ' + d.Nationality + '<br>Year: ' + d.Year + '   Time: ' + d.Time + '<br>' + d.Doping)
+                              .style('left', '350px')
+                              .style('top', '500px')
+                              .style('z-index', 2)
+                    })
+                    .on('mouseout', function(d) { 
+                        tooltip.transition().style('display', 'none')
+                    })
+                
+                // Append legend circles/text to svg
+                svg.append('circle')
+                   .attr('class', 'isDoper')
+                   .attr('cx', '500px')
+                   .attr('cy', '300px')
+                   .attr('r', 5)
+                
+                svg.append('text')
+                   .attr('x', '510px')
+                   .attr('y', '305px')
+                   .attr('class', 'legendText')
+                   .text('Riders with doping allegations')
+                
+                svg.append('circle')
+                   .attr('class', 'isNotDoper')
+                   .attr('cx', '500px')
+                   .attr('cy', '320px')
+                   .attr('r', 5)
+                
+                svg.append('text')
+                   .attr('x', '510px')
+                   .attr('y', '325px')
+                   .attr('class', 'legendText')
+                   .text('No doping allegations')
                 
                 // Append x axis label
-                
+                svg.append('text')
+                   .attr('class', 'label')
+                   .attr('text-anchor', 'end')
+                   .attr('x', svgWidth - chartPadding)
+                   .attr('y', svgHeight - 6)
+                   .text('Seconds Behind Best Time')
 
                 // Append y axis label
-                
-                
-                // Append x axis
-                
-                
-                // Append y axis
-                
+                svg.append('text')
+                   .attr('class', 'label')
+                   .attr('text-anchor', 'end')
+                   .attr('x', -40)
+                   .attr('y', chartPadding + 5)
+                   .attr('dy', '.75em')
+                   .attr('transform', 'rotate(-90)')
+                   .text('Ranking');
+
+                //Append x axis to svg
+                svg.append('g')
+                   .attr('transform', 'translate(0,' + (svgHeight - chartPadding) + ')')
+                   .call(xAxis);
+
+                // Append y axis to svg
+                svg.append('g')
+                   .attr('transform', 'translate(' + chartPadding + ', 0)')
+                   .call(yAxis);
             }
             buildChart(json);
         }
